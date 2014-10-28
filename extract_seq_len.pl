ffcba5e874eff31b2a2204b7437d5f6bcc6a42f9
Simon Stenberg <simon.stenberg@nmbu.no>
2014-10-28 11:48:49 +0100

    First commit

---
+++ extract_seq_len.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+
+#Use: script.pl file.fasta [length] > output.fasta  
+
+use strict;
+use warnings;
+use Bio::SeqIO;
+
+my $file = $ARGV[0];
+my $len = $ARGV[1];
+
+my $inseq = Bio::SeqIO->new(
+                                                        -file => "<$file",
+                                                        -format => "fasta",
+                                                        );
+my $outseq = Bio::SeqIO->new(
+                             -fh     => \*STDOUT,
+                             -format => "fasta",
+                             );
+
+while( my $seqref = $inseq->next_seq){
+
+        my $seq = $seqref->seq();
+        if (length($seq) >= $len){
+		$outseq->write_seq($seqref);
+		}
+
+}
+
+
+
+
+
+
+
