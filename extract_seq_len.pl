a9d04f1deeadb45ce1b916820752aca1e9faf4e4
Simon Stenberg <simon.stenberg@nmbu.no>
2014-10-28 11:50:35 +0100

    Fixed code formatting

--- extract_seq_len.pl
+++ extract_seq_len.pl
@@ -11,21 +11,20 @@ my $file = $ARGV[0];
 my $len = $ARGV[1];
 
 my $inseq = Bio::SeqIO->new(
-                                                        -file => "<$file",
-                                                        -format => "fasta",
-                                                        );
+	-file => "<$file",
+	-format => "fasta",
+);
+
 my $outseq = Bio::SeqIO->new(
-                             -fh     => \*STDOUT,
-                             -format => "fasta",
-                             );
+	-fh     => \*STDOUT,
+	-format => "fasta",
+);
 
 while( my $seqref = $inseq->next_seq){
-
-        my $seq = $seqref->seq();
+	my $seq = $seqref->seq();
         if (length($seq) >= $len){
 		$outseq->write_seq($seqref);
 		}
-
 }
 
 
