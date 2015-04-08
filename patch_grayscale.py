384248de0964c1f5a73b72480166d07c49b4ef63
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-04-08 12:57:59 +0200

    Moved optparse content
    The optparse content is only relevant when not importing the code.

--- patch_grayscale.py
+++ patch_grayscale.py
@@ -1,13 +1,6 @@
 import re
 from optparse import OptionParser as opt
 
-prsr = opt()
-prsr.add_option("-i", "--input", dest="input_file", metavar="FILE", help="Input pass.analaysis to patch")
-prsr.add_option("-g", "--grayScale", dest="gray_scale", metavar="STRING", help="WITHIN QUOTATION: New grayscale to patch with EXAMPLE: \"\'grayscale_values\': [230.0, 206.0, 193.0, 177.0, 164.0, 151.0, 136.0, 126.0, 114.0, 103.0, 93.0, 83.0, 74.0, 65.0, 56.0, 49.0, 43.0, 35.0, 29.0, 25.0, 21.0, 19.0, 16.0]\"")
-prsr.add_option("-o", "--output", dest="out", metavar="FILE", help="Output file, overwrites old by default")
-
-options, args = prsr.parse_args()
-
 def check_params(options):
 	
     if options.out == None:
@@ -39,5 +32,12 @@ def parser(self, input_file):
     return PASS_ANALYSIS
 
 if __name__ == "__main__":
+    
+    prsr = opt()
+    prsr.add_option("-i", "--input", dest="input_file", metavar="FILE", help="Input pass.analaysis to patch")
+    prsr.add_option("-g", "--grayScale", dest="gray_scale", metavar="STRING", help="WITHIN QUOTATION: New grayscale to patch with EXAMPLE: \"\'grayscale_values\': [230.0, 206.0, 193.0, 177.0, 164.0, 151.0, 136.0, 126.0, 114.0, 103.0, 93.0, 83.0, 74.0, 65.0, 56.0, 49.0, 43.0, 35.0, 29.0, 25.0, 21.0, 19.0, 16.0]\"")
+    prsr.add_option("-o", "--output", dest="out", metavar="FILE", help="Output file, overwrites old by default")
+
+    options, args = prsr.parse_args()
     
     patch(options.input_file, options.gray_scale, options.out)	
